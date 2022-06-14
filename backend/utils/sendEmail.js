f0961230f213d06a735eb5f1fdcc1e29251cd011
Devansh1412 <20ce061@charusat.edu.in>
2022-06-14 13:49:28 +0530

    first commit

---
+++ backend/utils/sendEmail.js
@@ -0,0 +1,44 @@
+// const nodeMailer = require("nodemailer");
+
+// const sendEmail = async(options)=>{
+//     const trasporter = nodeMailer.createTransport({
+//         service:process.env.SMPT_SERVICE,
+//         auth:{
+//             user:process.env.SMPT_MAIL,
+//             pass:process.env.SMPT_PASSWORD,
+//         },
+//     });
+//     const mailOptons = {
+//         from:process.env.SMPT_MAIL,
+//         to:process.env.SMPT_PASSWORD,
+//         subject: options.subject,
+//         text: options.message,
+//     }
+//     await trasporter.sendMail(mailOptons);
+
+// }
+// exports.module = sendEmail;
+const nodeMailer = require("nodemailer");
+
+const sendEmail = async (options) => {
+  const transporter = nodeMailer.createTransport({
+    host: process.env.SMPT_HOST,
+    port: process.env.SMPT_PORT,
+    service: process.env.SMPT_SERVICE,
+    auth: {
+      user: process.env.SMPT_MAIL,
+      pass: process.env.SMPT_PASSWORD,
+    },
+  });
+
+  const mailOptions = {
+    from: process.env.SMPT_MAIL,
+    to: options.email,
+    subject: options.subject,
+    text: options.message,
+  };
+
+  await transporter.sendMail(mailOptions);
+};
+
+module.exports = sendEmail;
